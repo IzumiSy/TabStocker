83c9664c6b439c8f8253d5d1a95db19ddb671b00
IzumiSy <beetle-noise@gmx.com>
2014-03-16 01:47:23 +0900

    Refactored background.js

--- background.js
+++ background.js
@@ -2,18 +2,24 @@
 // background.js
 
 const NOTIFY_ID = "default";
-var ItemsData = new Array();
 
 function isDuplicated(url)
 {
 	var r = false;
-	ItemsData = JSON.parse(localStorage["Items"]);
-	ItemsData.forEach(function(Item, i) {
+	JSON.parse(localStorage["Items"]).forEach(function(Item, i) {
 		if (Item["url"] == url) {
 			r = true;
 		}
 	});
 	return r;
+}
+
+function AddDataAndUpdateStorage(title, url)
+{
+	var Items = JSON.parse(localStorage["Items"]);
+	Items.push({ "title": title, "url": url });
+	localStorage["Items"] = JSON.stringify(Items);
+	chrome.browserAction.setBadgeText({text: String(Items.length)});
 }
 
 chrome.commands.onCommand.addListener(function(command) {
@@ -27,15 +33,9 @@ chrome.commands.onCommand.addListener(function(command) {
 	if (command == "stock-tab") {	
 		chrome.tabs.getSelected(window.id, function(tab) {
 			if (!isDuplicated(tab.url)) {
-				// AddItemAndUpdate(tab);
-				console.log("stock-tab");
 				opts["title"] = "A current tab was stocked";
 				opts["message"] = tab.title;
-				
-				ItemsData.push({ "title": tab.title, "url": tab.url });
-				localStorage["Items"] = JSON.stringify(ItemsData);
-				chrome.browserAction.setBadgeText({text: String(ItemsData.length)});
-				
+				AddDataAndUpdateStorage(tab.title, tab.url);
 			} else {
 				opts["title"] = "Error";
 				opts["message"] = "Any items are not allowed to be duplicated";
