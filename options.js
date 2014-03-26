d78b7640a297efe4a0c590ecfabbb958f402cfca
IzumiSy <beetle-noise@gmx.com>
2014-03-26 23:45:51 +0900

    Added codes around popup width settings

--- options.js
+++ options.js
@@ -5,6 +5,11 @@
 // The number below should be fixed manually to have it work well.
 const ui_menu_defaultPopupWidth = 250;
 
+// It works once at the first time to avoid blank of popup_width
+if (localStorage.PopupWidth == undefined || localStorage.PopupWidth < ui_menu_defaultPopupWidth) {
+	localStorage.PopupWidth = ui_menu_defaultPopupWidth;
+}
+
 document.body.onload = function() {
 	document.getElementById("popup_width").value = localStorage.PopupWidth;
 	document.getElementById("hide_favicon").checked = localStorage.HideFavicon == "true" ? true : false;
@@ -12,9 +17,15 @@ document.body.onload = function() {
 
 document.getElementById("save").onclick = function() {
 	var popup_width = document.getElementById("popup_width").value;
-	if (popup_width < ui_menu_defaultPopupWidth) {
+
+	// To set popup_width to zero, it's ganna be back to the default value.
+	if (popup_width > 0 && popup_width < ui_menu_defaultPopupWidth) {
+		alert("The value of Popup width should be bigger than 250px");
+		return;
+	} else if (popup_width == 0) {
 		popup_width = ui_menu_defaultPopupWidth;
 	}
+
 	localStorage.PopupWidth = popup_width;
 	localStorage.HideFavicon = document.getElementById("hide_favicon").checked;
 	window.close();
