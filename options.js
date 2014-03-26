7572cbe4d67873da34fd10ea819660a41332035a
IzumiSy <beetle-noise@gmx.com>
2014-03-26 23:27:34 +0900

    Added codes to load and save settings to options.js

--- options.js
+++ options.js
@@ -1,11 +1,18 @@
 
 // options.js
 
+// When you changed the default value for it in popup.html
+// The number below should be fixed manually to have it work well.
+const ui_menu_defaultPopupWidth = 250;
+
 document.body.onload = function() {
 	// load settings
+	document.getElementById("popup_width").value = localStorage.PopupWidth;
+	document.getElementById("hide_favicon").checked = localStorage.HideFavicon == "true" ? true : false;
 }
 
 document.getElementById("save").onclick = function() {
-	// save settings
+	localStorage.PopupWidth = document.getElementById("popup_width").value;
+	localStorage.HideFavicon = document.getElementById("hide_favicon").checked;
 	window.close();
 }
