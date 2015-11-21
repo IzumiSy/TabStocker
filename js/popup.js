bfeb27614f27afd82913840a4c76319364f56e3e
IzumiSy <beetle-noise@gmx.com>
2015-11-21 21:42:36 +0900

    Refactor popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,259 +1,276 @@
 
 // popup.js
 
-// API url for obtaining favicons
-const FAVICON_API = "http://favicon.hatena.ne.jp/?url=";
-
-// Tabs
-const ITEMS_SYNC_TAB = 0;
+const FAVICON_API     = "http://favicon.hatena.ne.jp/?url=";
+const ITEMS_SYNC_TAB  = 0;
 const ITEMS_LOCAL_TAB = 1;
 
-var BG = chrome.extension.getBackgroundPage();
+var BG         = chrome.extension.getBackgroundPage();
 var removeMode = false;
 
-////////////////////////////////////////////////
-
-function RemoveItemFromMenu(index, tab)
-{
-	$("#" + tab + " li:eq(" + index + ")").hide('slide', {direction: 'right'}, 200);
-}
-
-function SetItemToMenu(title, url, tab)
-{
-	var newAnchor = document.createElement("a");
-	var newList = document.createElement("li");
-	var newDiv = document.createElement("div");
-	var mainMenu = document.getElementById(tab);
-	var newFavicon;
-
-	// For favicon hide option
-	if (localStorage.getItem(BG.OPTION_HIDE_FAVICONS) != "true") {
-		newFavicon = document.createElement("img");
-		newFavicon.setAttribute("src", FAVICON_API + url);
-		newFavicon.setAttribute("class", "favicon");
-		newAnchor.appendChild(newFavicon);
-	}
-
-	newAnchor.appendChild(document.createTextNode(title));
-	newAnchor.setAttribute("class", "ui-corner-all");
-	newAnchor.setAttribute("role", "menuitem");
-	newAnchor.setAttribute("style", "width: " + $("body").width() - 12 + "px");
-	newList.appendChild(newAnchor);
-	newList.setAttribute("class", "ui-menu-item");
-	newList.setAttribute("role", "presentation");
-	newList.setAttribute("style", "width: " + $("body").width() - 12 + "px");
-	mainMenu.appendChild(newList);
-}
-
-function RestoreSavedItems()
-{
-	var Items = [];
-
-  // Local items
-	if (localStorage.getItem(BG.ITEMS_ID).length > 0) {
-		Items = JSON.parse(localStorage.getItem(BG.ITEMS_ID));
-		if (localStorage.getItem(BG.OPTION_AUTO_SORT) == "true") {
-		  Items = BG.utils.sorting(Items);
-		}
-		console.group("<< Previously stocked items (Local) >>");
-		Items.forEach(function(Item, i) {
-			if (Item) {
-				console.log(i + " " + Item["title"] + " :: " + Item["url"]);
-				SetItemToMenu(Item["title"], Item["url"], "items-local");
-			}
-		});
-		console.groupEnd();
-		chrome.browserAction.setBadgeText({text: String(Items.length)});
-	}
-
-	// Sync Items
-	Items = [];
-  chrome.storage.sync.get("items", function(data) {
-    if (!chrome.runtime.error) {
-      var d = data.items;
-      if (d !== undefined && data.items.length > 0) {
-        Items = data.items;
-      }
-      if (localStorage.getItem(BG.OPTION_AUTO_SORT) == "true") {
-		    Items = BG.utils.sorting(Items);
-		  }
-      console.group("<< Previously stocked items (Sync) >>");
-      Items.forEach(function(Item, i) {
-        if (Item) {
-          console.log(i + " " + Item["title"] + " :: " + Item["url"]);
-          SetItemToMenu(Item["title"], Item["url"], "items-sync");
+var stockItems = {
+  append: function(item) {
+  	this.applyUI.appendItem(item.title, item.url, BG.currentTab);
+  	BG.AddDataAndUpdateStorage(item.title, item.url, BG.currentTab);
+  },
+  
+  eliminate: function(item) {
+  	this.applyUI.eliminateItem(item["index"], BG.currentTab);
+  	BG.RemoveDataAndUpdateStorage(item["title"], BG.currentTab);
+  },
+  
+  launch: function(title) {
+    var items;
+  
+    if (BG.currentTab === "items-local") {
+    	items = JSON.parse(localStorage.getItem(BG.ITEMS_ID));
+    	stockItems.execute(items, title);
+  
+    } else { // === "items-sync"
+      chrome.storage.sync.get("items", function(data) {
+        if (!chrome.runtime.error) {
+          var d = data.items;
+          if (d !== undefined && data.items.length > 0) {
+            items = data.items;
+          }
+          stockItems.execute(items, title);
         }
       });
-      console.groupEnd();
     }
-  });
-}
-
-function AddItem(item)
-{
-	SetItemToMenu(item.title, item.url, BG.currentTab);
-	BG.AddDataAndUpdateStorage(item.title, item.url, BG.currentTab);
-}
-
-function RemoveItem(item)
-{
-	RemoveItemFromMenu(item["index"], BG.currentTab);
-	BG.RemoveDataAndUpdateStorage(item["title"], BG.currentTab);
-}
-
-function LaunchItem(title)
-{
-  var Items;
-
-  if (BG.currentTab === "items-local") {
-  	Items = JSON.parse(localStorage.getItem(BG.ITEMS_ID));
-  	launch(Items, title);
-
-  } else { // === "items-sync"
+  },
+  
+  execute: function(array, title) {
+    for (var i in array) {
+    	if (array[i]["title"] == title) {
+    	  chrome.tabs.create({url: array[i]["url"], selected: false});
+    		break;
+    	}
+    }
+  },
+  
+  restore: function() {
+  	var items = [];
+    var data = localStorage.getItem(BG.ITEMS_ID);  
+  
+    // Local items
+  	if (data !== undefined && data.length > 0) {
+  		items = JSON.parse(data);
+  		if (localStorage.getItem(BG.OPTION_AUTO_SORT) == "true") {
+  		  items = BG.utils.sorting(items);
+  		}
+  		console.group("<< Previously stocked items (Local) >>");
+  		items.forEach(function(item, i) {
+  			if (item) {
+  				console.log(i + " " + item["title"] + " :: " + item["url"]);
+  				stockItems.applyUI.appendItem(item["title"], item["url"], "items-local");
+  			}
+  		});
+  		console.groupEnd();
+  		chrome.browserAction.setBadgeText({text: String(items.length)});
+  	}
+  
+  	// Sync Items
+  	items = [];
     chrome.storage.sync.get("items", function(data) {
       if (!chrome.runtime.error) {
         var d = data.items;
         if (d !== undefined && data.items.length > 0) {
-          Items = data.items;
+          items = data.items;
         }
-        launch(Items, title);
+        if (localStorage.getItem(BG.OPTION_AUTO_SORT) == "true") {
+  		    items = BG.utils.sorting(items);
+  		  }
+        console.group("<< Previously stocked items (Sync) >>");
+        items.forEach(function(item, i) {
+          if (item) {
+            console.log(i + " " + item["title"] + " :: " + item["url"]);
+            stockItems.applyUI.appendItem(item["title"], item["url"], "items-sync");
+          }
+        });
+        console.groupEnd();
       }
     });
-  }
-}
-
-function launch(array, title)
-{
-  for (var i in array) {
-  	if (array[i]["title"] == title) {
-  	  chrome.tabs.create({url: array[i]["url"], selected: false});
-  		break;
+  },
+  
+  reorder: function(array) {
+  	var storageLength = array.length;
+    var targetList;
+  	var temps = [];
+  	var url;
+  
+    if (BG.currentTab == "items-local") {
+      targetList = $("#local li");
+    } else { // == "items-sync"
+      targetList = $("#sync li");
+    }
+  
+  	for (i = 0;i < targetList.length ;i++) {
+  		title = targetList[i].textContent;
+  		url = undefined;
+  		for (var j in array) {
+  			if (array[j]["title"] == title) {
+  				url = array[j]["url"];
+  				temps.push({"title": title, "url": url});
+  				break;
+  			}
+  		}
   	}
+  
+  	return temps;
+  },
+  
+  orderedSave: function() {
+  	var items;
+  
+  	if (BG.currentTab == "items-local") {
+  	  items = JSON.parse(localStorage.getItem(BG.ITEMS_ID));
+  	  localStorage.setItem(BG.ITEMS_ID, JSON.stringify(this.reorder(items)));
+  	} else { // == "items-sync"
+  	  chrome.storage.sync.get("items", function(data) {
+  	    var d = null;
+  	    if (!chrome.runtime.error) {
+  	      d = data.items;
+  	    }
+  	    if (d !== null && data.items.length > 0) {
+          items = data.items;
+        }
+        chrome.storage.sync.set({ "items": this.reorder(items) }, function() {
+          console.log("Reordered: items-sync");
+        });
+  	  });
+  	}
+  },
+  
+  applyUI: {
+    eliminateItem: function(index, tab) {
+    	$("#" + tab + " li:eq(" + index + ")").hide('slide', {direction: 'right'}, 200);
+    },
+    
+    appendItem: function(title, url, tab) {
+    	var newAnchor = document.createElement("a");
+    	var newList = document.createElement("li");
+    	var newDiv = document.createElement("div");
+    	var mainMenu = document.getElementById(tab);
+    	var newFavicon;
+    
+    	// For favicon hide option
+    	if (localStorage.getItem(BG.OPTION_HIDE_FAVICONS) != "true") {
+    		newFavicon = document.createElement("img");
+    		newFavicon.setAttribute("src", FAVICON_API + url);
+    		newFavicon.setAttribute("class", "favicon");
+    		newAnchor.appendChild(newFavicon);
+    	}
+    
+    	newAnchor.appendChild(document.createTextNode(title));
+    	newAnchor.setAttribute("class", "ui-corner-all");
+    	newAnchor.setAttribute("role", "menuitem");
+    	newAnchor.setAttribute("style", "width: " + $("body").width() - 12 + "px");
+    	newList.appendChild(newAnchor);
+    	newList.setAttribute("class", "ui-menu-item");
+    	newList.setAttribute("role", "presentation");
+    	newList.setAttribute("style", "width: " + $("body").width() - 12 + "px");
+    	mainMenu.appendChild(newList);
+    }
   }
-}
-
-function SaveReorderedList()
-{
-	var Items;
-
-	if (BG.currentTab == "items-local") {
-	  Items = JSON.parse(localStorage.getItem(BG.ITEMS_ID));
-	  localStorage.setItem(BG.ITEMS_ID, undefined);
-	  localStorage.setItem(BG.ITEMS_ID, JSON.stringify(reorder(Items)));
-	} else { // == "items-sync"
-	  chrome.storage.sync.get("items", function(data) {
-	    if (!chrome.runtime.error) {
-	      var d = data.items;
-	    }
-	    if (d !== undefined && data.items.length > 0) {
-        Items = data.items;
-      }
-      chrome.storage.sync.set({ "items": reorder(Items) }, function(){
-        console.log("Reordered: items-sync");
-      });
-	  });
-	}
-}
-
-function reorder(array)
-{
-	var storageLength = array.length;
-  var targetList;
-	var temps = [];
-	var url;
-
-  if (BG.currentTab == "items-local") {
-    targetList = $("#local li");
-  } else { // == "items-sync"
-    targetList = $("#sync li");
-  }
-
-	for (i = 0;i < targetList.length ;i++) {
-		title = targetList[i].textContent;
-		url = undefined;
-		for (var j in array) {
-			if (array[j]["title"] == title) {
-				url = array[j]["url"];
-				temps.push({"title": title, "url": url});
-				break;
-			}
-		}
-	}
-
-	return temps;
-}
-
-////////////////////////////////////////////////
-
-document.body.onload = function() {
-	BG.undefinedResolver()
-	$("body").css("font-size", localStorage.getItem(BG.OPTION_FONT_SIZE) + "em");
-	$("body").width(localStorage.getItem(BG.OPTION_POPUP_WIDTH));
-	$("ul").width($("body").width() - 4);
-	$("a.ui-menu-item").width($("body").width() - 12);
-
-	$("#add").button();
-	$("#options").button();
-	$("#remove").button();
-
-	$(".items").menu({
-		select: function(event, ui) {
-			if (removeMode) {
-				RemoveItem({"index": ui.item.index(), "title": ui.item.text()});
-			} else {
-				LaunchItem(ui.item.text());
-			}
-		}
-	});
-	if (localStorage.getItem(BG.OPTION_AUTO_SORT) == "false") {
-  	$(".items").sortable({
-  	  placeholder: "ui-state-highlight",
-  	  update: SaveReorderedList
-  	}).disableSelection();
-	}
-
-	// Height value loading should be placed just after saved items restoring
-	$(".ui-menu").height(localStorage.getItem(BG.OPTION_POPUP_HEIGHT));
-
-	// Settings for tab
-	var active_tab = ITEMS_SYNC_TAB;
-	if (BG.currentTab == "items-sync") {
-	  active_tab = ITEMS_LOCAL_TAB;
-	}
-	$("#tabs").tabs({
-	  activate: function(event, ui) {
-      if (ui.newPanel.selector === "#local") {
-        BG.currentTab = "items-local";
-      } else { // === "#sync"
-        BG.currentTab = "items-sync";
-      }
-      console.log("Tab switched: " + BG.currentTab);
-	  },
-	  active: active_tab
-	});
-	$(".ui-tabs-nav").width($("body").width() - 13);
-	$("#items-sync").width($("body").width() - 11).css("margin-top", "3px");
-	$("#items-local").width($("body").width() - 11).css("margin-top", "3px");
-
-	RestoreSavedItems();
-
-	// Resets a flag for remove mode
-	removeModeOn = false;
 };
 
-$("#add").on("click", function() {
-	chrome.tabs.getSelected(window.id, function (tab) {
-	  if (BG.currentTab === "items-local") {
-	    if (!BG.utils.isDuplicated(tab.url)) {
-	      AddItem(tab);
-	    } else {
-	      BG.errorNotification();
-	    }
-	  } else { // === "items-sync"
-  	  syncAddition(tab);
-	  }
-	});
-});
+var clickHandlers = {
+  btnAdd: function() {
+  	chrome.tabs.getSelected(window.id, function (tab) {
+  	  if (BG.currentTab === "items-local") {
+  	    console.log(tab.url);
+  	    if (!BG.utils.isDuplicated(tab.url)) {
+  	      stockItems.append(tab);
+  	    } else {
+  	      BG.notifications.error();
+  	    }
+  	  } else { // === "items-sync"
+    	  syncAddition(tab);
+  	  }
+  	});
+  },
+  
+  btnRemove: function() {
+  	var removeButton = document.getElementById("remove");
+  	if (removeButton.checked) {
+  		$(".ui-button-text").css("color", "red");
+  		$(".ui-menu").css("cursor", "pointer");
+  		$("#add").button("disable");
+  	} else {
+  		$(".ui-button-text").css("color", "black");
+  		$(".ui-menu").css("cursor", "default");
+  		$("#add").button("enable");
+  	}
+  	removeMode = ! removeMode;
+  },
+  
+  btnOption: function() {
+  	chrome.tabs.create({url: "options.html", selected: true});
+  }
+};
+
+var popupBodyHandlers = {
+  itemSelect: function(event, ui) {
+		if (removeMode) {
+			stockItems.eliminate({"index": ui.item.index(), "title": ui.item.text()});
+		} else {
+			stockItems.launch(ui.item.text());
+		}
+	},
+	
+	tabSwitch: function(event, ui) {
+    if (ui.newPanel.selector === "#local") {
+      BG.currentTab = "items-local";
+    } else { // === "#sync"
+      BG.currentTab = "items-sync";
+    }
+    console.log("Tab switched: " + BG.currentTab);
+  },
+	
+	onLoad: function() {
+  	BG.undefinedResolver();
+  	$("body").css("font-size", localStorage.getItem(BG.OPTION_FONT_SIZE) + "em");
+  	$("body").width(localStorage.getItem(BG.OPTION_POPUP_WIDTH));
+  	$("ul").width($("body").width() - 4);
+  	$("a.ui-menu-item").width($("body").width() - 12);
+  
+  	$("#add").button();
+  	$("#add").on("click", clickHandlers.btnAdd);
+  	$("#remove").button();
+  	$("#remove").on("click", clickHandlers.btnRemove);
+  	$("#options").button();
+  	$("#options").on("click", clickHandlers.btnOption);
+  
+  	$(".items").menu({ select: popupBodyHandlers.itemSelect });
+  	if (localStorage.getItem(BG.OPTION_AUTO_SORT) == "false") {
+    	$(".items").sortable({
+    	  placeholder: "ui-state-highlight",
+    	  update: stockItems.orderedSave
+    	}).disableSelection();
+  	}
+  
+  	// Height value loading should be placed just after saved items restoring
+  	$(".ui-menu").height(localStorage.getItem(BG.OPTION_POPUP_HEIGHT));
+  
+  	// Settings for tab
+  	var active_tab = ITEMS_SYNC_TAB;
+  	if (BG.currentTab == "items-sync") {
+  	  active_tab = ITEMS_LOCAL_TAB;
+  	}
+  	$("#tabs").tabs({
+  	  activate: popupBodyHandlers.tabSwitch,
+  	  active: active_tab
+  	});
+  	$(".ui-tabs-nav").width($("body").width() - 13);
+  	$("#items-sync").width($("body").width() - 11).css("margin-top", "3px");
+  	$("#items-local").width($("body").width() - 11).css("margin-top", "3px");
+  
+  	stockItems.restore();
+  
+  	// Resets a flag for remove mode
+  	removeModeOn = false;
+  }
+};
 
 // BG.isDuplicated() cannot be used for sync tab
 function syncAddition(tab)
@@ -268,29 +285,13 @@ function syncAddition(tab)
           }
         });
         if (r === true) {
-          BG.errorNotification();
+          BG.notifications.error();
           return;
         }
       }
-      AddItem(tab);
+      stockItems.append(tab);
     }
   });
 }
 
-$("#remove").on("click", function() {
-	var removeButton = document.getElementById("remove");
-	if (removeButton.checked) {
-		$(".ui-button-text").css("color", "red");
-		$(".ui-menu").css("cursor", "pointer");
-		$("#add").button("disable");
-	} else {
-		$(".ui-button-text").css("color", "black");
-		$(".ui-menu").css("cursor", "default");
-		$("#add").button("enable");
-	}
-	removeMode = ! removeMode;
-});
-
-$("#options").on("click", function() {
-	chrome.tabs.create({url: "options.html", selected: true});
-});
+document.body.onload = popupBodyHandlers.onLoad;
