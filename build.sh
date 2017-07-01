001bfa954b11beb94c6c5a5051dc3f4d8d5b5ba2
IzumiSy <beetle-noise@gmx.com>
2017-07-01 11:30:31 +0900

    Update

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #/bin/sh
 
-browserify src/background.js -t babelify --outfile ./dists/background.js && \
-  browserify src/options.js -t babelify --outfile ./dists/options.js && \
-  browserify src/popup.js -t babelify --outfile ./dists/popup.js
+html-minifier src/options.html -o dists/options.html -c ./_html-minifier.json
+html-minifier src/popup.html -o dists/popup.html -c ./_html-minifier.json
+
+browserify src/background.js -t babelify --outfile ./dists/background.js
+browserify src/options.js -t babelify --outfile ./dists/options.js
+browserify src/popup.js -t babelify --outfile ./dists/popup.js
