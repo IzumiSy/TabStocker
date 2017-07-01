755e2bc5b0dcd6690787006dc9a76b8bede0897a
IzumiSy <beetle-noise@gmx.com>
2017-07-01 11:47:09 +0900

    Install uglify-js and modify build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,7 @@ html-minifier src/popup.html -o dists/popup.html -c ./_html-minifier.json
 browserify src/background.js -t babelify --outfile ./dists/background.js
 browserify src/options.js -t babelify --outfile ./dists/options.js
 browserify src/popup.js -t babelify --outfile ./dists/popup.js
+
+uglifyjs dists/background.js -o ./dists/background.js
+uglifyjs dists/options.js -o ./dists/options.js
+uglifyjs dists/popup.js -o ./dists/popup.js
