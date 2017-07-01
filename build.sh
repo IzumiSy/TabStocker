4981117ec9da2add69c8f0a3120653de6c1c7001
IzumiSy <beetle-noise@gmx.com>
2017-07-01 11:52:08 +0900

    Tweak build script

--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
 #/bin/sh
 
-html-minifier src/options.html -o dists/options.html -c ./_html-minifier.json
-html-minifier src/popup.html -o dists/popup.html -c ./_html-minifier.json
+html-minifier src/options.html -o dists/options.html -c _html-minifier.json
+html-minifier src/popup.html -o dists/popup.html -c _html-minifier.json
+echo "HTML minification has done"
 
-browserify src/background.js -t babelify --outfile ./dists/background.js
-browserify src/options.js -t babelify --outfile ./dists/options.js
-browserify src/popup.js -t babelify --outfile ./dists/popup.js
+browserify src/background.js -t babelify --outfile dists/background.js
+browserify src/options.js -t babelify --outfile dists/options.js
+browserify src/popup.js -t babelify --outfile dists/popup.js
+echo "JS transforming with browserify has done"
 
-uglifyjs dists/background.js -o ./dists/background.js
-uglifyjs dists/options.js -o ./dists/options.js
-uglifyjs dists/popup.js -o ./dists/popup.js
+uglifyjs dists/background.js -o dists/background.js
+uglifyjs dists/options.js -o dists/options.js
+uglifyjs dists/popup.js -o dists/popup.js
+echo "JS uglification has done"
+
+cleancss src/styles.css -o dists/styles.css
+echo "CSS cleaning has done"
