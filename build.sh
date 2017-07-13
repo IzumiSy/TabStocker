3cf7f53023c767275ac8c49e2d0b8c2ca4f42ec1
IzumiSy <beetle-noise@gmx.com>
2017-07-13 12:17:42 +0900

    Rename because of naming collision of chrome extension

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
-html-minifier src/options.html -o dists/options.html -c _html-minifier.json
-html-minifier src/popup.html -o dists/popup.html -c _html-minifier.json
+html-minifier src/options.html -o dists/options.html -c .html-minifier.json
+html-minifier src/popup.html -o dists/popup.html -c .html-minifier.json
 echo "HTML minification has done"
 
 browserify src/background.js -t babelify --outfile dists/background.js
