428abcd099e96ece6ad926e5b31f41882527d95f
IzumiSy <beetle-noise@gmx.com>
2017-07-20 22:21:39 +0900

    Add notifications

---
+++ src/notifications.js
@@ -0,0 +1,30 @@
+const NOTIFY_ID = 'default';
+
+export default notifications = {
+  error() {
+    chrome.notifications.create(NOTIFY_ID, {
+      type: 'basic',
+      title: 'TabStocker: Error',
+      message: 'Tab already stocked',
+      iconUrl: 'assets/error.png',
+    }, () => {
+      // TODO 
+    });
+  },
+
+  success(title) {
+    if (!title) {
+      console.log('Error: notification.success() needs a page title');
+      return;
+    }
+    chrome.notifications.create(NOTIFY_ID, {
+      type: 'basic',
+      title: 'TabStocker: Success',
+      message: title,
+      iconUrl: 'assets/main.png',
+    }, () => {
+      // TODO
+    });
+  },
+};
+
